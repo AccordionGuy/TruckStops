dc5511ec0b3d71e8fceebc55baf19661cfab2507
Joey deVilla <joey@joeydevilla.com>
2017-05-13 14:13:21 -0400

    Tracking button working properly now; just need to get it to update its text properly!

--- Truck Stops/MapViewController.m
+++ Truck Stops/MapViewController.m
@@ -12,6 +12,7 @@
 
 @interface MapViewController ()
 
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *trackingButton;
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *viewSelector;
 @property (weak, nonatomic) IBOutlet UIButton *currentLocationButton;
@@ -40,6 +41,7 @@ const int METERS_PER_MILE = 1609.34;
   bool userIsReadingDetails;
 
   BOOL firstRun;
+  BOOL touchDetected;
 }
 
 - (void)viewDidLoad {
@@ -48,6 +50,7 @@ const int METERS_PER_MILE = 1609.34;
   NSLog(@"ViewDidLoad");
 
   firstRun = true;
+  touchDetected = NO;
   [self testInternetConnection];
 
   locationManager = [[CLLocationManager alloc] init];
@@ -74,11 +77,8 @@ const int METERS_PER_MILE = 1609.34;
   [self.activityIndicator stopAnimating];
 
   // TODO: Get this values from UserDefaukts
-  currentTrackingMode = kTrackingOn;
-
+  [self turnTrackingModeOn];
   userIsReadingDetails = NO;
-
-//  [self performSelector:@selector(centerMapOnCurrentLocation) withObject:self afterDelay:1];
 }
 
 
@@ -100,13 +100,36 @@ const int METERS_PER_MILE = 1609.34;
   }
 }
 
+- (IBAction)trackingButtonTapped:(UIBarButtonItem *)sender {
+  NSLog(@"trackingButtonTapped");
+
+  if (currentTrackingMode != kTrackingOn) {
+    [self turnTrackingModeOn];
+  } else {
+    [self turnTrackingModeOff];
+  }
+}
+
+- (IBAction)searchButtonTapped:(UIBarButtonItem *)sender {
+  NSLog(@"Current tracking mode: %u", currentTrackingMode);
+}
+
+- (void)turnTrackingModeOn {
+  NSLog(@"Tracking ON");
+  currentTrackingMode = kTrackingOn;
+  [self.mapView setUserTrackingMode:MKUserTrackingModeFollowWithHeading animated:YES];
+  [self.trackingButton setTitle:@"Tracking on"];
+}
+
+- (void)turnTrackingModeOff {
+  NSLog(@"Tracking OFF");
+  currentTrackingMode = kTrackingOff;
+  [self.mapView setUserTrackingMode:MKUserTrackingModeNone animated:YES];
+  [self.trackingButton setTitle:@"Tracking off"];
+}
+
 - (IBAction)currentLocationButtonTapped:(UIButton *)sender {
-  [self centerMapOnCurrentLocation];
-//  [self displayCurrentLocationAtDefaultZoom];
-//  int radius = 100;
-//  [self getTruckStopDataForLatitude:currentLocation.coordinate.latitude
-//                       andLongitude:currentLocation.coordinate.longitude
-//                  withRadiusInMiles:radius];
+  [self centerMapOnCurrentLocationAtDefaultZoom];
 }
 
 - (void)getTruckStopDataForLatitude:(double)latitude
@@ -157,7 +180,6 @@ const int METERS_PER_MILE = 1609.34;
     NSLog(@"Number of truck stops: %lu", numTruckStops);
 
     if (numTruckStops > 0) {
-      NSLog(@"Adding annotations");
       [self addTruckStops:truckStops toMapView:self.mapView];
     }
   }];
@@ -203,40 +225,47 @@ const int METERS_PER_MILE = 1609.34;
                   withRadiusInMiles:radius];
 }
 
-- (void)centerMapOnLocation:(CLLocation *)location withZoomRadius:(double)radius {
-  MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(location.coordinate,
-                                                                 radius * 2.0,
-                                                                 radius * 2.0);
+- (void)centerMapOnCurrentLocationAtDefaultZoom {
+  const CLLocationDistance DEFAULT_RADIUS = (CLLocationDistance)milesToMeters(100.0);
+  MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(currentLocation.coordinate,
+                                                                 DEFAULT_RADIUS * 2.0,
+                                                                 DEFAULT_RADIUS * 2.0);
   [self.mapView setRegion:region animated:YES];
 }
 
-- (void)centerMapOnCurrentLocation {
-  const CLLocationDistance DEFAULT_RADIUS = (CLLocationDistance)milesToMeters(100.0);
-  [self centerMapOnLocation:currentLocation withZoomRadius:DEFAULT_RADIUS];
+- (void)centerMap {
+    NSLog(@"centerMap");
+    [self.mapView setCenterCoordinate:self.mapView.userLocation.location.coordinate animated:YES];
+}
+
+- (void)centerMapAndTurnTrackingOn {
+  [self centerMap];
+  currentTrackingMode = kTrackingOn;
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
   NSLog(@"Touches began");
+  touchDetected = YES;
   lastKnownRegion = self.mapView.region;
 
   if (currentTrackingMode == kTrackingOn) {
     currentTrackingMode = kTrackingTemporarilyOff;
+    [self.mapView setUserTrackingMode:MKUserTrackingModeNone animated:YES];
   }
 }
 
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
   NSLog(@"Touches ended");
-  if (currentTrackingMode == kTrackingTemporarilyOff) {
-    [self performSelector:@selector(centerMap) withObject:self afterDelay:5];
-    currentTrackingMode = kTrackingOn;
+  touchDetected = NO;
+  if (currentTrackingMode != kTrackingOff) {
+    [NSObject cancelPreviousPerformRequestsWithTarget:self];
+    self.mapView.userTrackingMode = MKUserTrackingModeNone;
+    [self performSelector:@selector(turnTrackingModeOn) withObject:self afterDelay:5];
   }
 }
 
 
 #pragma mark - MKMapViewDelegate
-
-- (void)mapView:(MKMapView *)mapView regionWillChangeAnimated:(BOOL)animated {
-}
 
 // Map position or zoom level changed
 - (void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated {
@@ -269,9 +298,13 @@ const int METERS_PER_MILE = 1609.34;
   lastKnownCenterCoordinate = mapView.centerCoordinate;
   lastKnownRegion = mapView.region;
 
-  if (currentTrackingMode == kTrackingTemporarilyOff) {
-    [self performSelector:@selector(centerMap) withObject:self afterDelay:5];
-    currentTrackingMode = kTrackingOn;
+  if (touchDetected) {
+    touchDetected = NO;
+    if (currentTrackingMode != kTrackingOff) {
+      [NSObject cancelPreviousPerformRequestsWithTarget:self];
+      self.mapView.userTrackingMode = MKUserTrackingModeNone;
+      [self performSelector:@selector(turnTrackingModeOn) withObject:self afterDelay:5];
+    }
   }
 }
 
@@ -316,11 +349,6 @@ const int METERS_PER_MILE = 1609.34;
   CLLocation *leftEdgeCenter = [[CLLocation alloc] initWithLatitude:center.latitude longitude:(center.longitude - span.longitudeDelta * 0.5)];
   CLLocation *rightEdgeCenter = [[CLLocation alloc] initWithLatitude:center.latitude longitude:(center.longitude + span.longitudeDelta * 0.5)];
   return [leftEdgeCenter distanceFromLocation:rightEdgeCenter];
-}
-
-- (void)centerMap {
-//  NSLog(@"centerMap");
-//  [self.mapView setCenterCoordinate:self.mapView.userLocation.location.coordinate animated:YES];
 }
 
 - (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
@@ -390,12 +418,12 @@ const int METERS_PER_MILE = 1609.34;
   }
 }
 
-- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control {
-//  id<MKAnnotation> annotation = view.annotation;
-//  if ([annotation isKindOfClass:[TruckStopAnnotation class]]) {
-//    TruckStopAnnotation *truckStopAnnotation = (TruckStopAnnotation *)annotation;
-//    NSLog(@"TAPPED! %@", truckStopAnnotation.title);
-//  }
+- (void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view {
+  if (currentTrackingMode != kTrackingOff) {
+    [NSObject cancelPreviousPerformRequestsWithTarget:self];
+    self.mapView.userTrackingMode = MKUserTrackingModeNone;
+    [self performSelector:@selector(turnTrackingModeOn) withObject:self afterDelay:20];
+  }
 }
 
 
@@ -430,10 +458,10 @@ const int METERS_PER_MILE = 1609.34;
 }
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
-  NSLog(@"didUpdateLocations");
+  NSLog(@"locationManager:didUpdateLocations");
   currentLocation = locations.lastObject;
   if (firstRun) {
-    [self centerMapOnCurrentLocation];
+    [self centerMapOnCurrentLocationAtDefaultZoom];
     [self getTruckStopDataForLatitude:currentLocation.coordinate.longitude
                          andLongitude:currentLocation.coordinate.latitude
                     withRadiusInMiles:100.0];
@@ -441,15 +469,10 @@ const int METERS_PER_MILE = 1609.34;
   }
 }
 
-- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
-
-}
-
 
 #pragma mark - Internet
 
-- (void)testInternetConnection
-{
+- (void)testInternetConnection {
   networkReachableDetector = [Reachability reachabilityWithHostname:@"www.appleiphonecell.com"];
 
   // Internet is reachable
@@ -485,3 +508,4 @@ double metersToMiles(double meters) {
 }
 
 @end
+
